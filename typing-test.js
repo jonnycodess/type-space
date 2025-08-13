b4e21d78768c01b38e7f18001e8d22c19e554529
jonnycs <jberry4@bearcubs.santarosa.edu>
2025-08-14 04:28:14 +0500

    Add countdown timer

--- typing-test.js
+++ typing-test.js
@@ -1,15 +1,29 @@
-    function getCharsInput(testArea) {
-      let value = testArea.value;
-      return value.length;
-    }
+function getCharsInput(testArea) {
+  let value = testArea.value;
+  return value.length;
+}
 
-    // Gross WPM = (chars typed/5) / (time in seconds/60)
-    function calcGrossWPM(charsInput) {
-      let chars = charsInput;
-      let timeInSeconds = 30;
-      return console.log(((chars/5) / (timeInSeconds/60)));
-    }
+// Gross WPM = (chars typed/5) / (time in seconds/60)
+function calcGrossWPM(charsInput) {
+  let chars = charsInput;
+  let timeInSeconds = 30;
+  return console.log(((chars/5) / (timeInSeconds/60)));
+}
 
-    let testArea = document.querySelector('.test-area');
-    let calcButton = document.querySelector('.calc-btn');
-    calcButton.addEventListener('click', () => calcGrossWPM(getCharsInput(testArea)));
+function updateCountDown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  countdownEl.innerHTML = `${minutes}:${seconds}`;
+  time--;
+}
+
+let testArea = document.querySelector('.test-area');
+let calcButton = document.querySelector('.calc-btn');
+calcButton.addEventListener('click', () => calcGrossWPM(getCharsInput(testArea)));
+
+const startingMinutes = 0.5;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountDown, 1000);
